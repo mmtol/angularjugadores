85a9ac853e6448fac8d0086557c7cb890b05ecc1
mmtol <mmigueltolosana@gmail.com>
2025-11-13 11:36:03 +0100

    jugadores

--- src/components/jugadores.component/jugadores.component.ts
+++ src/components/jugadores.component/jugadores.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Params } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { Jugador } from '../../models/Jugador';
+import ServiceFutbol from '../../services/service.futbol';
 
-@Component({
+@Component
+({
   selector: 'app-jugadores',
   standalone: false,
   templateUrl: './jugadores.component.html',
   styleUrl: './jugadores.component.css',
 })
-export class JugadoresComponent {
+export class JugadoresComponent implements OnInit
+{
+  public jugadores!:Array<Jugador>;
 
+  constructor(private _activeRoute:ActivatedRoute,
+              private _service:ServiceFutbol) {}
+
+  ngOnInit(): void 
+  {
+    this._activeRoute.params.subscribe((parametros:Params)=>
+    {
+      if (parametros["nombre"] != null)
+      {
+        let nombre = parametros["nombre"];
+
+        this._service.findJugadores(nombre).then(response =>
+        {
+          this.jugadores = response;
+        })
+      }
+    })
+  }
 }
