96a3d2ab7caf2ba453c1e0ceb11ad316c7948b0d
mmtol <mmigueltolosana@gmail.com>
2025-11-13 11:24:01 +0100

    menu y services

--- src/components/menu.component/menu.component.ts
+++ src/components/menu.component/menu.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Equipo } from '../../models/Equipo';
+import ServiceFutbol from '../../services/service.futbol';
+import { Router } from '@angular/router';
 
-@Component({
+@Component
+({
   selector: 'app-menu',
   standalone: false,
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit
+{
+  public equipos!:Array<Equipo>;
+  public nombre!:string;
 
+  @ViewChild("cajaNombre") cajaNombre:ElementRef;
+
+  constructor(private _service:ServiceFutbol,
+              private _router:Router)
+  {
+    this.cajaNombre = new ElementRef("");
+  }
+
+  ngOnInit(): void 
+  {
+    this._service.getEquipos().then(response =>
+    {
+      this.equipos = response;
+    })
+  }
+
+  buscarJugadores()
+  {
+    let nombre = this.cajaNombre.nativeElement.value;
+    this._router.navigate(["/jugadores", nombre]);
+  }
 }
--- src/services/service.futbol.ts
+++ src/services/service.futbol.ts
@@ -1,8 +1,69 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { environment } from "../environments/environment.development";
 
 @Injectable()
 export default class ServiceFutbol
 {
     constructor(private _http:HttpClient){}
+
+    getEquipos():Promise<any>
+    {
+        let url = environment.futbol;
+        let endPoint = "api/Equipos";
+        let promise = new Promise((resolve) =>
+        {
+            this._http.get(url+endPoint).subscribe(response =>
+            {
+                resolve(response);
+            })
+        })
+
+        return promise;
+    }
+
+    findEquipo(idEquipo:number):Promise<any>
+    {
+        let url = environment.futbol;
+        let endPoint = "api/Equipos/"+idEquipo;
+        let promise = new Promise((resolve) =>
+        {
+            this._http.get(url+endPoint).subscribe(response =>
+            {
+                resolve(response);
+            })
+        })
+
+        return promise;
+    }
+
+    getJugadoresEquipo(idEquipo:number):Promise<any>
+    {
+        let url = environment.futbol;
+        let endPoint = "api/Jugadores/JugadoresEquipo/"+idEquipo;
+        let promise = new Promise((resolve) =>
+        {
+            this._http.get(url+endPoint).subscribe(response =>
+            {
+                resolve(response);
+            })
+        })
+
+        return promise;
+    }
+
+    findJugadores(nombre:string):Promise<any>
+    {
+        let url = environment.futbol;
+        let endPoint = "api/Jugadores/BuscarJugadores/"+nombre;
+        let promise = new Promise((resolve) =>
+        {
+            this._http.get(url+endPoint).subscribe(response =>
+            {
+                resolve(response);
+            })
+        })
+
+        return promise;
+    }
 }
